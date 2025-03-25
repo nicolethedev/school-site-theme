610689db165ba8230883d64e6310895527c85275
Nicole <nicolelogan@hotmail.com>
2025-03-24 19:57:38 -0700

    adding lightbox to home page

---
+++ assets/js/lightgallery-init.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const galleryElements = document.querySelectorAll(".wp-block-image");
+
+  if (galleryElements.length) {
+    galleryElements.forEach((gallery) => {
+      const img = gallery.querySelector("img");
+
+      if (img) {
+        const imgSrc = img.getAttribute("src");
+        const imgAlt = img.getAttribute("alt") || "";
+
+        if (!gallery.querySelector("a")) {
+          const link = document.createElement("a");
+          link.href = imgSrc;
+          link.classList.add("lightbox");
+          link.setAttribute("data-src", imgSrc);
+          link.setAttribute("data-sub-html", `<h4>${imgAlt}</h4>`);
+          img.parentNode.insertBefore(link, img);
+          link.appendChild(img);
+        }
+      }
+    });
+
+    lightGallery(document.body, {
+      selector: ".wp-block-image a",
+      plugins: [lgZoom, lgThumbnail],
+      download: false,
+      closable: true,
+      closeOnTap: true,
+      escKey: true,
+      controls: true,
+      counter: true,
+      getCaptionFromTitleOrAlt: true,
+      showCloseIcon: true,
+      enableSwipe: true,
+      enableDrag: true,
+      speed: 500,
+    });
+  }
+});
--- functions.php
+++ functions.php
@@ -55,6 +55,55 @@ function add_student_image_sizes_to_dropdown($sizes) {
 }
 add_filter('image_size_names_choose', 'add_student_image_sizes_to_dropdown');
 
+function enqueue_lightgallery_on_homepage() {
+    if (is_front_page()) { // Only load on the front page
+        // Load lightGallery CSS from CDN
+        wp_enqueue_style(
+            'lightgallery-css',
+            'https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/2.7.1/css/lightgallery-bundle.min.css',
+            array(),
+            '2.7.1'
+        );
+
+        // Load lightGallery JS from CDN
+        wp_enqueue_script(
+            'lightgallery-js',
+            'https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/2.7.1/lightgallery.umd.min.js',
+            array('jquery'),
+            '2.7.1',
+            true // Loads in footer for better performance
+        );
+
+        // Load lightGallery Zoom plugin
+        wp_enqueue_script(
+            'lightgallery-zoom',
+            'https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/2.7.1/plugins/zoom/lg-zoom.min.js',
+            array('lightgallery-js'),
+            '2.7.1',
+            true
+        );
+
+        // Load lightGallery Thumbnail plugin
+        wp_enqueue_script(
+            'lightgallery-thumbnail',
+            'https://cdnjs.cloudflare.com/ajax/libs/lightgallery-js/2.7.1/plugins/thumbnail/lg-thumbnail.min.js',
+            array('lightgallery-js'),
+            '2.7.1',
+            true
+        );
+
+        // Load custom JS file to initialize lightGallery
+        wp_enqueue_script(
+            'custom-lightgallery-settings',
+            get_template_directory_uri() . '/assets/js/lightgallery-init.js',
+            array('lightgallery-js', 'lightgallery-zoom', 'lightgallery-thumbnail'),
+            null,
+            true
+        );
+    }
+}
+add_action('wp_enqueue_scripts', 'enqueue_lightgallery_on_homepage');
+
 
 
 
