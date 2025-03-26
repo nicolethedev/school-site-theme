4d8ce7efd70f810606cd94133e87741311072a2a
Nicole <nicolelogan@hotmail.com>
2025-03-26 10:39:52 -0700

    school address block

---
+++ school-site-blocks/school-address/build/blocks-manifest.php
@@ -0,0 +1,30 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'school-address' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'school-site-blocks/school-address',
+		'version' => '0.1.0',
+		'title' => 'School Address',
+		'category' => 'text',
+		'icon' => 'location',
+		'description' => 'The school address',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'attributes' => array(
+			'svgIcon' => array(
+				'type' => 'boolean',
+				'default' => false
+			)
+		),
+		'textdomain' => 'school-address',
+		'editorScript' => 'file:./index.js',
+		'style' => 'file:./style-index.css',
+		'render' => 'file:./render.php'
+	)
+);
---
+++ school-site-blocks/school-address/school-address.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name:       School Address
+ * Description:       The school address
+ * Version:           0.1.0
+ * Requires at least: 6.7
+ * Requires PHP:      7.4
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       school-address
+ *
+ * @package SchoolSiteBlocks
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function school_site_blocks_school_address_block_init() {
+	if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) { // Function introduced in WordPress 6.8.
+		wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
+	} else {
+		if ( function_exists( 'wp_register_block_metadata_collection' ) ) { // Function introduced in WordPress 6.7.
+			wp_register_block_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
+		}
+		$manifest_data = require __DIR__ . '/build/blocks-manifest.php';
+		foreach ( array_keys( $manifest_data ) as $block_type ) {
+			register_block_type( __DIR__ . "/build/{$block_type}" );
+		}
+	}
+}
+add_action( 'init', 'school_site_blocks_school_address_block_init' );
